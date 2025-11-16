649f5f7702241f023a5a370e1315f986ea26d66e
sanbei101 <sanbei101@outlook.com>
2025-11-16 12:55:48 +0800

    3

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,22 @@
 import * as core from "@actions/core";
-import * as exec from "@actions/exec";
+import { sync } from "./sync";
 
-export type SyncOpts = {
-  registry: string;
-  namespace: string;
-  username: string;
-  password: string;
-  images: string[];
-};
-export function Src2Dst(image: string, opts: SyncOpts) {
-  const [name, tag = "latest"] = image.split(":");
-
-  const parts = name.split("/");
-  if (parts.length > 2) {
-    parts.shift(); // 处理如 ghcr.io/bitnami/redis:8
-  }
-  const dst = parts.join("/");
-
-  return `${opts.registry}/${opts.namespace}/${dst}:${tag}`;
-}
-
-async function docker(cmd: string[]): Promise<void> {
+async function run(): Promise<void> {
   try {
-    await exec.exec("docker", cmd);
-  } catch (e: any) {
-    core.error(`docker ${cmd.join(" ")} failed: ${e.message}`);
-    throw e;
+    const registry = core.getInput("registry", { required: true });
+    const namespace = core.getInput("namespace", { required: true });
+    const username = core.getInput("username", { required: true });
+    const password = core.getInput("password", { required: true });
+    const images = core
+      .getInput("images", { required: true })
+      .split(/\r?\n/)
+      .map((s) => s.trim())
+      .filter(Boolean);
+
+    await sync({ registry, namespace, username, password, images });
+  } catch (error: any) {
+    core.setFailed(error.message);
   }
 }
 
-export async function sync(opts: SyncOpts): Promise<void> {
-  const { registry, username, password, images } = opts;
-
-  core.info(`🔐 login ${registry}`);
-  await exec.exec("docker", [
-    "login",
-    "-u",
-    username,
-    "-p",
-    password,
-    registry,
-  ]);
-
-  for (const img of images) {
-    try {
-      const src = img,
-        dst = Src2Dst(img, opts);
-      core.info(`🚚 ${src} -> ${dst}`);
-      await docker(["pull", src]);
-      await docker(["tag", src, dst]);
-      await docker(["push", dst]);
-      await docker(["image", "rm", src, dst]);
-    } catch (e: any) {
-      core.warning(`image: ${img} sync failed: ${e.message}, continue...`);
-    }
-  }
-
-  core.info("✅ all images synced!");
-}
+run();
